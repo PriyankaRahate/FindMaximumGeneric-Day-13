8bbed0e2df3fd9d79cc8e1ea60e7560d6e6b3a9f
unknown <priyanka.rahate23@gmail.com>
2022-04-05 12:12:24 +0530

    intmaxProgram

---
+++ src/genericDay13/IntMaximumGeneric.java
@@ -0,0 +1,14 @@
+package genericDay13;
+
+public class IntMaximumGeneric {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the program to find maximum problem using Generics");
+
+	}
+
+
+
+	}
+
+
